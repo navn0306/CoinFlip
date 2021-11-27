b311284aa9c1f41c9aeb59db5b40bad82f510ad6
Navneet <navn0306@gmail.com>
2021-11-27 17:24:26 +0530

    Add: Added Program to Flip Coin and print percentage of Heads and Tails

--- src/com/bridge/Flipcoin/FlipCoin.java
+++ src/com/bridge/Flipcoin/FlipCoin.java
@@ -1,8 +1,39 @@
 package com.bridge.Flipcoin;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class FlipCoin {
     public static void main(String[] args) {
-        System.out.println(" \n Welcome, This is a program to Flip Coin and print percentage of Heads and Tails ");
-    }
+        int heads = 0;
+        int tails = 0;
+        int counter = 1;
+        double randNum;
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("How many times will the coin be flipped? ");
+        int flips = in.nextInt();
+
+        while (counter <= flips) {
+            randNum = Math.random();
+
+            if (randNum < 0.5) {
+                heads++;
+            } else {
+                tails++;
+            }
+            counter++;
+        }
+        System.out.println();
+        System.out.println("Number of Heads = " + heads);
+        System.out.println("Number of Tails = " + tails);
+
+        double headsPercentage = (heads * 100) / flips;
+        double tailsPercentage = (tails * 100) / flips;
+
+        System.out.println("Heads Percentage " + headsPercentage);
+        System.out.println("Tails Percentage " + tailsPercentage);
+
+
+    }
 }
