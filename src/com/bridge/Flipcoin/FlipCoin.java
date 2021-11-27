7993537d88ddc6e128f740e8da77b0728c0aeb2e
Navneet <navn0306@gmail.com>
2021-11-27 17:18:52 +0530

    ADD: Added Welcome message

---
+++ src/com/bridge/Flipcoin/FlipCoin.java
@@ -0,0 +1,8 @@
+package com.bridge.Flipcoin;
+
+public class FlipCoin {
+    public static void main(String[] args) {
+        System.out.println(" \n Welcome, This is a program to Flip Coin and print percentage of Heads and Tails ");
+    }
+
+}
